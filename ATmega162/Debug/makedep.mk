dff6d677b7920c47dd0f1acd999d2b6f32c11d64
ingunnjv <ingunnjv@itk-d657.win.ntnu.no>
2017-09-13 08:34:39 +0200

    created atmel studio project

---
+++ ATmega162/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\drivers\sram.c
+
+src\drivers\uart.c
+
+src\main.c
+
